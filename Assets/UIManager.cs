86594e2b7a4b3d943079cb7714e3955b8e3b8d04
gabinales <136241817+gabinales@users.noreply.github.com>
2023-06-26 17:38:59 -0300

    display de status, sistema de diálogos e menu do inventário

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public PlayerStats playerStats;
     [SerializeField] private TMP_Text hpText;
+    [SerializeField] private Image playerStatsPanel;
+
+    private float timerInatividade;
+    public float thresholdInatividade = 15f;
+    Color ativoTransparencia = new Color(1f,1f,1f,0.8f);
+    Color inativoTransparencia = new Color(1f,1f,1f,0.5f);
+
+    private GameController gameController;
 
     private void OnEnable(){ // Executado quando o componente é habilitado
         playerStats.HpAlterado += AtualizaHpText; // inscreve o método AtualizaHpText, que será executado somente quando que o evento HpAlterado for acionado
@@ -16,8 +25,32 @@ public class UIManager : MonoBehaviour
     }
     
     void Start(){
+        gameController = FindObjectOfType<GameController>();
         AtualizaHpText(playerStats.Hp);
-    } 
+    }
+
+    private void Update(){
+        if(gameController == null || gameController.state != GameState.MovimentacaoLivre){
+            // Se o jogo não estiver no estado de Movimentação Livre, não rastreie a inatividade
+            Debug.Log("Deu gamestate null.");
+            return;
+        }
+        if(Input.anyKeyDown || Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
+            timerInatividade = 0f;
+            AlteraTransparencia(ativoTransparencia);
+        }
+        else{
+            timerInatividade += Time.deltaTime;
+
+            if(timerInatividade >= thresholdInatividade){
+                AlteraTransparencia(inativoTransparencia);
+            }
+        }
+    }
+    private void AlteraTransparencia(Color transparencia){
+        playerStatsPanel.color = transparencia;
+    }
+
     private void AtualizaHpText(int novoHp){
         hpText.text = "HP: " + novoHp.ToString();
     }
